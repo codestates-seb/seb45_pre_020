330f741fafe624eea7dfebddc7c7d16f3855e672
dalsun <ddy07068@gmail.com>
2023-08-11 17:55:33 +0900

    header 구현 완료

--- Client/stackoverflowclient/src/components/header/header.js
+++ Client/stackoverflowclient/src/components/header/header.js
@@ -1,6 +1,7 @@
 import Logo from '../../atoms/logo/logo';
 import './header.css';
 import '../../atoms/button/button.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -9,9 +10,15 @@ function Header() {
         <Logo />
       </div>
       <div className="button_container">
-        <button className="ask_button">Ask Question</button>
-        <button className="login_button">Log in</button>
-        <button className="signup_button">Sign up</button>
+        <Link to="/ask">
+          <button className="ask_button">Ask Question</button>
+        </Link>
+        <Link to="/login">
+          <button className="login_button">Log in</button>
+        </Link>
+        <Link to="/signup">
+          <button className="signup_button">Sign up</button>
+        </Link>
       </div>
     </header>
   );
