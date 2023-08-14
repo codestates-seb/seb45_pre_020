f1d1ca741dda4a0e66e06df40aae7a552e563ccf
Givehim <manndu125@gmail.com>
2023-08-14 09:18:05 +0900

    Search 구현 CSS 높이 크기 재수정

--- Client/stackoverflowclient/src/App.js
+++ Client/stackoverflowclient/src/App.js
@@ -1,7 +1,7 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/header/header';
 // import Footer from './components/Footer';
-// import Main from './Pages/Main/Main';
+import Main from './Pages/Main/Main';
 // import Login from './Pages/Login';
 // import Signup from './Pages/Signup';
 // import Post from './Pages/Post';
@@ -13,16 +13,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      {/* 
+      
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} />
         <Route pate="/signup" element={<Signup />} />
         <Route path="/post/:id" element={<Post />} />
-        <Route path="/ask" element={<Ask />} />
+        <Route path="/ask" element={<Ask />} /> */}
       </Routes>
-      {/* 
-      <Footer /> */}
+      
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
