fc807eea47a0c297e7f265dd0534495fe185ee6f
sintobury <herkongho@gmail.com>
2023-08-21 15:59:54 +0900

    logo에 modifymode init기능추가

--- Client/stackoverflowclient/src/atoms/logo/logo.js
+++ Client/stackoverflowclient/src/atoms/logo/logo.js
@@ -1,9 +1,20 @@
 import './logo.css';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setModifyMode } from '../../redux/reducers/modifySlice';
 
 const Logo = () => {
+  const dispatch = useDispatch();
+  const modifyMode = useSelector((state) => state.modifySlice.modifyMode);
   return (
-    <Link to="/">
+    <Link
+      to="/"
+      onClick={() =>
+        dispatch(
+          setModifyMode({ modifyMode: !modifyMode, post_id: '', type: '' }),
+        )
+      }
+    >
       <img src="/img/logo.svg" alt="logo" />
     </Link>
   );
